5a92d88a8bc3753d27611a4c3fe161bb4daf3c4a
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-16 14:23:42 +0530

    Add function to calculate sum and demonstrate struct usage in main

--- main.go
+++ main.go
@@ -2,9 +2,30 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello,World")
+// Function that returns the sum of two numbers
+func add(a int, b int) int {
+	return a + b
 }
 
-//*No arguments: Unlike some languages (e.g., C’s int main(int argc, char *argv[])), Go’s main() takes no arguments and returns nothing. It’s simple by desig
-//*If your package is main, you must have a main() function, or Go will complain when you try to build it.
+// Struct example
+type Person struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	// For loop example
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Iteration:", i)
+	}
+
+	// Calling a function
+	sum := add(10, 20)
+	fmt.Println("Sum of 10 and 20 is:", sum)
+
+	// Creating and printing a struct
+	person := Person{Name: "Nandhu", Age: 22}
+	fmt.Printf("Person: %+v\n", person)
+}
